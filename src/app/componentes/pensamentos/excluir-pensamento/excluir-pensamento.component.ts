405deb436911a5dc333d17e61775eccac91bdfc4
Kaique Guilherme <kaiqueguilhermepereiramiranda@gmail.com>
2023-04-03 00:13:36 -0300

    Memoteca de Pensamentos, esta faltando metodo de Excluir e editar

---
+++ src/app/componentes/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
@@ -0,0 +1,45 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+
+@Component({
+  selector: 'app-excluir-pensamento',
+  templateUrl: './excluir-pensamento.component.html',
+  styleUrls: ['./excluir-pensamento.component.css']
+})
+export class ExcluirPensamentoComponent implements OnInit {
+
+  pensamento: Pensamento = {
+    id: 0,
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute 
+  ) { }
+
+  ngOnInit(): void {
+    
+    const id = this.route.snapshot.paramMap.get('id')
+    this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => {
+      this.pensamento = pensamento
+    })
+  }
+
+  excluirPensamento(){
+    this.service.excluir(this.pensamento.id).subscribe(() =>{
+      this.router.navigate(['/listarPensamento'])
+    })
+  }
+
+  Cancelar(){
+    this.router.navigate(['/listarPensamento']) 
+  }
+
+
+}
